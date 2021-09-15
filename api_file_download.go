f4d3baa9cf20bb71e35180c0d1beb63f437c39a8
chyroc <chyroc@qq.com>
2021-09-15 16:17:19 +0800

    feat: download file show progress

--- api_file_download.go
+++ api_file_download.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 
 	"github.com/chyroc/go-aliyundrive/internal/helper_tool"
+	"github.com/vbauerster/mpb/v5"
+	"github.com/vbauerster/mpb/v5/decor"
 )
 
 // GetFile 获取文件信息
@@ -47,42 +49,24 @@ func (r *FileService) DownloadFile(ctx context.Context, request *DownloadFileReq
 		return err
 	}
 
-	f, err := ioutil.TempFile("", "ali-drive-*-"+request.FileID)
+	file, err := downloadURL(res.URL, request.ShowProgressBar)
 	if err != nil {
 		return err
 	}
-	req, err := http.NewRequest(http.MethodGet, res.URL, nil)
-	if err != nil {
-		return err
-	}
-	req.Header.Set("User-Agent", userAgent)
-	req.Header.Set("Referer", "https://www.aliyundrive.com/")
-	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
-	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
 
-	response, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	defer response.Body.Close()
-
-	if _, err = io.Copy(f, response.Body); err != nil {
-		return err
-	}
-
-	if err = os.Rename(f.Name(), distName); err != nil {
+	if err = os.Rename(file, distName); err != nil {
 		return err
 	}
 	return nil
 }
 
 type DownloadFileReq struct {
-	DriveID      string                   `json:"drive_id"`
-	FileID       string                   `json:"file_id"`
-	Dist         string                   `json:"dist"`          // 如果此值不为空，则下载文件到这个位置
-	DistDir      string                   `json:"dist_dir"`      // 如果此值不为空，则下载文件到 `DistDir`/<name> 位置
-	ConflictType DownloadFileConflictType `json:"conflict_type"` // 如果目标文件已存在，处理方式：报错，覆盖，自动重命名，默认是自动重命名
+	DriveID         string                   `json:"drive_id"`
+	FileID          string                   `json:"file_id"`
+	Dist            string                   `json:"dist"`              // 如果此值不为空，则下载文件到这个位置
+	DistDir         string                   `json:"dist_dir"`          // 如果此值不为空，则下载文件到 `DistDir`/<name> 位置
+	ConflictType    DownloadFileConflictType `json:"conflict_type"`     // 如果目标文件已存在，处理方式：报错，覆盖，自动重命名，默认是自动重命名
+	ShowProgressBar bool                     `json:"show_progress_bar"` // 展示下载进度条
 }
 
 type DownloadFileConflictType int
@@ -93,4 +77,56 @@ const (
 	DownloadFileConflictTypeError      DownloadFileConflictType = 2
 )
 
-var downloadHttpCli = &http.Client{}
+var downloadHttpClient = http.Client{}
+
+func downloadURL(url string, showProgressBar bool) (string, error) {
+	f, err := ioutil.TempFile("", "ali-drive-*")
+	if err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("User-Agent", userAgent)
+	req.Header.Set("Referer", "https://www.aliyundrive.com/")
+	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
+
+	resp, err := downloadHttpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if showProgressBar {
+		fileSize := resp.ContentLength
+		progress := mpb.New(mpb.WithWidth(20))
+		bar := progress.AddBar(
+			fileSize,
+			// 进度条前的修饰
+			mpb.PrependDecorators(
+				decor.Name("[download] "),
+				decor.CountersKibiByte("% .2f / % .2f"), // 已下载数量
+				decor.Percentage(decor.WCSyncSpace),     // 进度百分比
+			),
+			// 进度条后的修饰
+			mpb.AppendDecorators(
+				decor.EwmaSpeed(decor.UnitKiB, "% .2f", 60),
+			),
+		)
+		reader := bar.ProxyReader(resp.Body)
+		defer reader.Close()
+
+		if _, err := io.Copy(f, reader); err != nil {
+			return "", err
+		}
+	} else {
+		if _, err := io.Copy(f, resp.Body); err != nil {
+			return "", err
+		}
+	}
+
+	return f.Name(), nil
+}
--- go.mod
+++ go.mod
@@ -8,13 +8,17 @@ require (
 	github.com/sirupsen/logrus v1.8.1
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	github.com/stretchr/testify v1.7.0
+	github.com/vbauerster/mpb/v5 v5.4.0
 )
 
 require (
+	github.com/VividCortex/ewma v1.1.1 // indirect
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/juju/go4 v0.0.0-20160222163258-40d72ab9641a // indirect
 	github.com/juju/persistent-cookiejar v0.0.0-20171026135701-d5e5a8405ef9 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/net v0.0.0-20210825183410-e898025ed96a // indirect
 	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
