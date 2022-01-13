ecca131eb53a788e1eb1033b3e48a2261b04dcf2
hellojukay <hellojukay@163.com>
2022-01-13 11:06:51 +0800

    取消临时文件

--- api_file_download.go
+++ api_file_download.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
@@ -49,12 +48,8 @@ func (r *FileService) DownloadFile(ctx context.Context, request *DownloadFileReq
 		return err
 	}
 
-	file, err := downloadURL(res.URL, request.ShowProgressBar)
+	err = downloadURL(res.URL, distName, request.ShowProgressBar)
 	if err != nil {
-		return err
-	}
-
-	if err = os.Rename(file, distName); err != nil {
 		return err
 	}
 	return nil
@@ -79,15 +74,15 @@ const (
 
 var downloadHttpClient = http.Client{}
 
-func downloadURL(url string, showProgressBar bool) (string, error) {
-	f, err := ioutil.TempFile("", "ali-drive-*")
+func downloadURL(url string, filename string, showProgressBar bool) error {
+	f, err := os.Create(filename)
 	if err != nil {
-		return "", err
+		return err
 	}
 
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		return "", err
+		return  err
 	}
 	req.Header.Set("User-Agent", userAgent)
 	req.Header.Set("Referer", "https://www.aliyundrive.com/")
@@ -96,7 +91,7 @@ func downloadURL(url string, showProgressBar bool) (string, error) {
 
 	resp, err := downloadHttpClient.Do(req)
 	if err != nil {
-		return "", err
+        return err
 	}
 	defer resp.Body.Close()
 
@@ -120,13 +115,12 @@ func downloadURL(url string, showProgressBar bool) (string, error) {
 		defer reader.Close()
 
 		if _, err := io.Copy(f, reader); err != nil {
-			return "", err
+            return err
 		}
 	} else {
 		if _, err := io.Copy(f, resp.Body); err != nil {
-			return "", err
+            return err
 		}
 	}
-
-	return f.Name(), nil
+    return nil
 }
